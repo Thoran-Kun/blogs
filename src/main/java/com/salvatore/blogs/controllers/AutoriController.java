69d778a05a51c550d53f52bad78bdddc90531b46
Salvatore <pepesalvatore12@gmail.com>
2026-02-10 16:48:19 +0100

    add author entities and controllers

---
+++ src/main/java/com/salvatore/blogs/controllers/AutoriController.java
@@ -0,0 +1,4 @@
+package com.salvatore.blogs.controllers;
+
+public class AutoriController {
+}
--- src/main/java/com/salvatore/blogs/entities/Autore.java
+++ src/main/java/com/salvatore/blogs/entities/Autore.java
@@ -1,4 +1,31 @@
 package com.salvatore.blogs.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.Random;
+
+@Getter
+@Setter
+@ToString
 public class Autore {
+    private long id;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String dataDiNascita;
+    private String avatar;
+
+
+    public Autore(String nome, String cognome, String email, String dataDiNascita, String avatar) {
+        Random rndm = new Random();
+        this.id = rndm.nextInt(1, 1000);
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.dataDiNascita = dataDiNascita;
+        this.avatar = "https://ui-avatars.com/api/?name=" + nome + "+" + cognome;
+    }
 }
